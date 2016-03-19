592e52715705ce92d111d49099b2547902bca0eb
Alan Jui <alanjui.1960@gmail.com>
2016-03-19 15:51:51 +0800

    加入「Add a link」功能

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 import {bootstrap} from './lib/bootstrap.js';
 
 $(document).ready(function () {
-  alert('Document Loaded!!');
+  // alert('Document Loaded!!');
 
-  $('.js-like').on('click', (event) => {
+  $('.js-like').on('click', function(event) {
+    // console.log('.js-like is click');
+    
     event.preventDefault();
-    $(this).text('Liked!');  
+    
+    // $(this)
+    //   .text('Liked!')
+    //   .addClass('is-liked');
+    
+    $(this)
+      .text('Liked')  
+      .closest('.news-item')
+      .addClass('is-liked');
+  });
+
+  $('.js-add-link').on('click', function(event) {
+    event.preventDefault();
+    
+    $('.js-form').toggleClass('is-visible');
   });
 });
 
