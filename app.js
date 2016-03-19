9a439ef09bc6973d5774bc8c8e0b5f2ed8b89698
Alan Jui <alanjui.1960@gmail.com>
2016-03-19 17:44:33 +0800

    加入「顯示 Posted by 」功能

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import {bootstrap} from './lib/bootstrap.js';
 $(document).ready(function () {
   // alert('Document Loaded!!');
 
+  //------------------------------------------------
+  // Like
+  //------------------------------------------------
   $('.js-like').on('click', function(event) {
     // console.log('.js-like is click');
     
@@ -18,11 +21,30 @@ $(document).ready(function () {
       .addClass('is-liked');
   });
 
+  //------------------------------------------------
+  // Add Link
+  //------------------------------------------------
   $('.js-add-link').on('click', function(event) {
     event.preventDefault();
     
     $('.js-form').toggleClass('is-visible');
   });
+  
+  //------------------------------------------------
+  // Display Modal Dialog Box
+  //------------------------------------------------
+  $('.js-show-modal').on('click', function(event) {
+    event.preventDefault();
+    
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+  
+  $('.js-modal-overlay').on('click', function(event) {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
+  });
+  
 });
 
 
